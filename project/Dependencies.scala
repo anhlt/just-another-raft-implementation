599b5428576aae01e4d398a7bc531a4327f5194f
Scala Steward <scala-steward@example.com>
2025-05-27 16:13:09 +0000

    Update cats-effect, cats-effect-kernel, ... to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,21 +1,19 @@
 import sbt._
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
-
 object Dependencies {
 
+  val catsCore         = Def.setting("org.typelevel" %% "cats-core" % "2.13.0")
+  val log4CatsCore     = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.0")
+  val log4CatsLog4j    = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.0")
+  val catsEffect       = Def.setting("org.typelevel" %% "cats-effect" % "3.6.1")
+  val catsEffectKernel = Def.setting("org.typelevel" %% "cats-effect-kernel" % "3.6.1")
+  val catsEffectStd    = Def.setting("org.typelevel" %% "cats-effect-std" % "3.6.1")
 
-  val catsCore = Def.setting("org.typelevel" %% "cats-core" % "2.13.0")
-  val log4CatsCore= Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.0")
-  val log4CatsLog4j = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.0")
-  val catsEffect = Def.setting("org.typelevel" %% "cats-effect" % "3.5.7")
-  val catsEffectKernel = Def.setting("org.typelevel" %% "cats-effect-kernel" % "3.5.7")
-  val catsEffectStd = Def.setting("org.typelevel" %% "cats-effect-std" % "3.5.7")
-
-  val munit = Def.setting("org.scalameta" %% "munit" % "1.1.0" % Test)
-  val munitCatsEffect = Def.setting("org.typelevel" %% "munit-cats-effect" % "2.0.0" % Test)
-  val scalaCheck = Def.setting("org.scalacheck" %% "scalacheck" % "1.16.0" % Test)
-  val munitScalaCheck = Def.setting("org.scalameta" %% "munit-scalacheck" % "1.1.0" % Test)
+  val munit            = Def.setting("org.scalameta" %% "munit" % "1.1.0" % Test)
+  val munitCatsEffect  = Def.setting("org.typelevel" %% "munit-cats-effect" % "2.0.0" % Test)
+  val scalaCheck       = Def.setting("org.scalacheck" %% "scalacheck" % "1.16.0" % Test)
+  val munitScalaCheck  = Def.setting("org.scalameta" %% "munit-scalacheck" % "1.1.0" % Test)
   val scalaCheckEffect = Def.setting("org.typelevel" %% "scalacheck-effect" % "2.0.0-M2" % Test)
 
   val munitScalaCheckEffect = Def.setting(
