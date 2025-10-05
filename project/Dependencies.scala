ba355622702e00f70f7e18c37cb90a9c9301aae8
Scala Steward <scala-steward@example.com>
2025-10-05 00:23:49 +0000

    Update logback-classic to 1.5.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val catsMtl          = Def.setting("org.typelevel" %% "cats-mtl" % "1.6.0")
   val log4CatsCore     = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.1")
   val log4CatsLog4j    = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.1" % Test)
-  val logback          = Def.setting("ch.qos.logback" % "logback-classic" % "1.5.18" % Test)
+  val logback          = Def.setting("ch.qos.logback" % "logback-classic" % "1.5.19" % Test)
   val catsEffect       = Def.setting("org.typelevel" %% "cats-effect" % "3.6.3")
   val catsEffectKernel = Def.setting("org.typelevel" %% "cats-effect-kernel" % "3.6.3")
   val catsEffectStd    = Def.setting("org.typelevel" %% "cats-effect-std" % "3.6.3")
