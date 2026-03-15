507b5db7dd2ce7401489f60432a05f742c2ec1ea
Scala Steward <scala-steward@example.com>
2026-03-15 00:36:24 +0000

    Update log4cats-slf4j to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
 
   val catsCore         = Def.setting("org.typelevel" %% "cats-core" % "2.13.0")
   val catsMtl          = Def.setting("org.typelevel" %% "cats-mtl" % "1.6.0")
-  val log4CatsCore     = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.1")
-  val log4CatsLog4j    = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.7.1" % Test)
+  val log4CatsCore     = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.8.0")
+  val log4CatsLog4j    = Def.setting("org.typelevel" %% "log4cats-slf4j" % "2.8.0" % Test)
   val logback          = Def.setting("ch.qos.logback" % "logback-classic" % "1.5.18" % Test)
   val catsEffect       = Def.setting("org.typelevel" %% "cats-effect" % "3.6.3")
   val catsEffectKernel = Def.setting("org.typelevel" %% "cats-effect-kernel" % "3.6.3")
